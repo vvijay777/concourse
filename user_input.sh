df45957f32d9fa1d03a8b98d776884700736f574
vvijay777 <sftesting7777@gmail.com>
2021-01-12 12:10:41 +0530

    Update user_input.sh

--- user_input.sh
+++ user_input.sh
@@ -2,7 +2,7 @@ alb=$(cat properties.cfg | grep asg | cut -d'=' -f2)
 asg=$(cat properties.cfg | grep asg | cut -d'=' -f2)
 echo $asg
 echo $alb
-more asg.json
+cat asg.json
 sed -i "/^[[:space:]]*\"url\"/ s/:.*/:\ \"\http:\/\/$alb\"/" asg.json
 sed -i "/^[[:space:]]*\"asg_names\"/ s/:.*/:\ [\"$asg\"],/" asg.json
-more asg.json
+cat asg.json
